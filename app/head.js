445d19935b5e6145d522c67f51adc1697e4a7909
RealOtakuBoi <sohamseth2005@gmail.com>
2022-12-03 23:05:56 +0530

    Navbar kinda done

--- app/head.js
+++ app/head.js
@@ -2,7 +2,6 @@ const Head = () => (
   <>
     <title>OtakuVerse</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="/favicon.ico" />
   </>
 );
 
