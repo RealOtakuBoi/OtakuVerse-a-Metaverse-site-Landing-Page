49aceca2e028b9a48b363f0bbcbf703d7820c497
RealOtakuBoi <sohamseth2005@gmail.com>
2022-12-04 13:54:55 +0530

    Get Started done

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -1,6 +1,16 @@
-const StartSteps = () => (
-  <div>
-    start steps
+
+import styles from "../styles";
+
+
+
+const StartSteps = ({number , text}) => (
+  <div className={`${styles.flexCenter} flex-row`}>
+      <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5d]`}>
+        <p className='font-bold text-[24px] text-white'>{number}</p>
+      </div>
+      <p className="flex-1 ml-[30px] text-[18px] font-normal text-white leading-[32px]">
+        {text}
+      </p>
   </div>
 );
 
