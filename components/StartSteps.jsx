34a1fd2a55dad46858895ebdf615dc7da396fcc4
RealOtakuBoi <sohamseth2005@gmail.com>
2022-12-04 16:26:16 +0530

    Esling disabled during build

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -1,16 +1,15 @@
-
 import styles from "../styles";
 
-
-
-const StartSteps = ({number , text}) => (
+const StartSteps = ({ number, text }) => (
   <div className={`${styles.flexCenter} flex-row`}>
-      <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5d]`}>
-        <p className='font-bold text-[24px] text-white'>{number}</p>
-      </div>
-      <p className="flex-1 ml-[30px] text-[18px] font-normal text-white leading-[32px]">
-        {text}
-      </p>
+    <div
+      className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5d]`}
+    >
+      <p className="font-bold text-[24px] text-white">{number}</p>
+    </div>
+    <p className="flex-1 ml-[30px] text-[18px] font-normal text-white leading-[32px]">
+      {text}
+    </p>
   </div>
 );
 
--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: false,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
